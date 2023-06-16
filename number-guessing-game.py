60acff45e2c3c83cdba4b4e6d757060783028a0f
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-16 14:48:47 +0100

    Add additional hints in number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -31,6 +31,15 @@ for i in range(guesses_allowed):
     # Check if the player has reached their number of guesses limit and end the game if so
     if (i == guesses_allowed -1):
         print('Sorry, you have run out of guesses. You lose!')
+    # Add additional hints
+    if abs(guess - answer) <= 10:
+        print("You're warm!")
+    elif abs(guess-answer) <= 20:
+        print("You're getting warmer!")
+    elif abs(guess - answer) <= 30:
+        print("You're cold.")
+    else:
+        print("You're freezing.")
 
 # If the code executes past the while loop the user has guessed the correct answer
 print('Congratulations! You guessed the correct number. You win!')
