7013085405b80650205041ce520d97e099d7c9a2
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-16 14:22:23 +0100

    Ask the player to guess a number in number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -3,3 +3,9 @@ import random
 # Generate a random number between 1 and 50
 guess_range = 50
 answer = random.randint(1, guess_range)
+
+# Start the game and ask the player to guess the number
+print('Welcome to the number guessing game!')
+print('')
+user_input = input(f"Guess a number between 1 and {guess_range} : \n")
+guess = int(user_input)
