689329c7c498a0cb9500eda08819a4c75b405532
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-16 14:25:19 +0100

    Add a while loop for incorrect user input in number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -9,3 +9,10 @@ print('Welcome to the number guessing game!')
 print('')
 user_input = input(f"Guess a number between 1 and {guess_range} : \n")
 guess = int(user_input)
+
+# Compare the users input to the random number generated and check if it matches
+# While the user has not guessed the correct answer re ask them to guess again
+guess = ""
+while guess != answer:
+    user_input = input(f"Guess a number between 1 and {guess_range} : \n")
+    guess = int(user_input)
